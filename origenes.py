6ee6a24efbc60281074fd9592e2e18f15e0bd6e7
nachh07 <39889859+nachh07@users.noreply.github.com>
2022-04-30 09:58:25 -0300

    Update origenes.py

--- origenes.py
+++ origenes.py
@@ -2,10 +2,19 @@ from librerias import pyodbc, pd, exception
 from querys import productos
 server = 'DESKTOP-TS5PPCK' 
 database = 'BDVentas' 
-df = pd.read_csv("C:\\Users\\SID\\Downloads\\clientes.csv", sep=";")
-df_clientes = df.drop_duplicates(subset=['codigo','nombre','apellido','localidad'])
+df = pd.read_csv("https://datasets.datos.mincyt.gob.ar/dataset/personal-de-ciencia-y-tecnologia/archivo/8ab77b16-f1a8-4d3f-b664-67becf83a9b9", sep=";")
+conn_dbventas = pyodbc.connect('driver={SQL SERVER};server='+server+';database='+database+';trusted_connections=YES')
+
+#Añadimos la columna cod_cliente al dataframe para insertar los datos de COD_ID de la tabla clientes de la base de datos
+df = df.assign(cod_cliente='')
+
+#reacomodamos las columnas del df clientes
+df_clientes = df['cod_cliente','nombre','apellido','provincia', 'sexo_id', 'edad', 'edad', 'provincia']
+
+#Borramos duplicados en caso que existan 
+df_clientes = df.drop_duplicates(subset=['cod_cliente','nombre','apellido','provincia', 'sexo_id', 'edad', 'edad'])
+
 try: 
-    conn_dbventas = pyodbc.connect('driver={SQL SERVER};server='+server+';database='+database+';trusted_connections=YES')
     cursor = conn_dbventas.cursor()
     query = cursor.execute(productos)
     data = cursor.fetchall()
@@ -13,7 +22,3 @@ try:
     cursor.close()
 except exception as e:
         print(f"Error: '{e}'")
-
-
-
-
